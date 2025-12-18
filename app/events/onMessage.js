44fe1d658de0be8fddd620350d6616e89a23992d
codingpoppy94 <yly0617@naver.com>
2025-12-18 20:00:26 +0900

    feat: 멀티 리플파일 처리

--- app/events/onMessage.js
+++ app/events/onMessage.js
@@ -12,72 +12,68 @@ module.exports = {
   async execute(client, msg) {
     const prefix = "!";
     if (msg.author.bot) return;
+    // 첨부파일이 있는 경우
     if (msg.attachments.size > 0) {
-      if (msg.guild.id === "922118764437340230") return;   // 우끼대 서버는 무시
-      const fileUrl = msg.attachments.first().url;
-      const fileName = msg.attachments.first().name;
-      const guildId = stringUtils.encodeGuildId(msg.guild.id);
-      const createUser = msg.member.nickname || msg.author.username;
+      if (msg.guild.id === "922118764437340230") return;
 
-      // rofl 파일 아닌 경우 무시
-      if (!fileName.endsWith(".rofl")) return;
+      // 모든 첨부파일을 순회하며 처리
+      for (const [id, attachment] of msg.attachments) {
+        const fileName = attachment.name;
+        const fileUrl = attachment.url;
 
-      // rofl 확장자 제거
-      const fileNameWithoutExt = fileName.endsWith(".rofl")
-        ? fileName.slice(0, -5)
-        : fileName;
+        if (!fileName.endsWith(".rofl")) continue; // .rofl 아니면 다음 파일로
 
-      // 대회 리플 파일인지 일반 리플 파일인지 구분
-      if (championShipCheck(fileName)) {
+        const guildId = stringUtils.encodeGuildId(msg.guild.id);
+        const createUser = msg.member.nickname || msg.author.username;
+        const fileNameWithoutExt = fileName.slice(0, -5);
+
         try {
-          result = await championShipService.save(
-            fileUrl,
-            fileNameWithoutExt,
-            createUser,
-            guildId,
-            2
-          );
-          msg.reply(result);
-          return;
-        } catch (error) {
-          msg.reply(error.message);
-          return;
-        }
-      } 
-
-      // 일반 리플 파일인 경우
-      if (fileNameCheck(fileName)) {
-        try {
-          result = await replayService.save(
-            fileUrl,
-            fileNameWithoutExt,
-            createUser,
-            guildId,
-            1
-          );
+          let result;
+          if (championShipCheck(fileName)) {
+            // 대회용 리플 처리
+            result = await championShipService.save(
+              fileUrl,
+              fileNameWithoutExt,
+              createUser,
+              guildId,
+              2
+            );
+          } else if (fileNameCheck(fileName)) {
+            // 일반 리플 처리
+            result = await replayService.save(
+              fileUrl,
+              fileNameWithoutExt,
+              createUser,
+              guildId,
+              1
+            );
+          } else {
+            // 형식 불일치
+            msg.reply(
+              `:red_circle:등록실패: ${fileNameWithoutExt} 잘못된 리플 파일 형식`
+            );
+            continue;
+          }
           msg.reply(result);
         } catch (error) {
-          msg.reply(error.message);
+          msg.reply(`오류 발생(${fileNameWithoutExt}): ${error.message}`);
         }
-      } else {
-        msg.reply(
-          `:red_circle:등록실패: ${fileNameWithoutExt} 잘못된 리플 파일 형식`
-        );
       }
-    } else {
-      if (!msg.content.startsWith(prefix)) return;
-      if (msg.content.slice(0, prefix.length) !== prefix) return;
+      return;
+    }
 
-      const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-      const command = args.shift().toLowerCase();
+    if (!msg.content.startsWith(prefix)) return;
+    if (msg.content.slice(0, prefix.length) !== prefix) return;
 
-      try {
-        let cmd = client.commands.get(command);
-        if (cmd) cmd.run(client, msg, args); // 명령어 실행
-      } catch (error) {
-        console.error(error);
-        msg.reply("명령어 실행 중 오류가 발생했습니다.");
-      }
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+      let cmd = client.commands.get(command);
+      if (cmd) cmd.run(client, msg, args); // 명령어 실행
+    } catch (error) {
+      console.error(error);
+      msg.reply("명령어 실행 중 오류가 발생했습니다.");
     }
   },
 };
@@ -92,4 +88,4 @@ const fileNameCheck = (fileName) => {
 const championShipCheck = (fileName) => {
   const regex = new RegExp(/^[a-zA-Z0-9]*_\d{4}_\d{4}_champs\.rofl$/);
   return regex.test(fileName);
-}
+};
