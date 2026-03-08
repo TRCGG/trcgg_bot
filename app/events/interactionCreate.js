609f9400a2f4afb5443680dba4316b6fc9b20a6e
codingpoppy94 <yly0617@naver.com>
2026-03-09 00:56:19 +0900

    필요없는 모듈 참조 삭제

--- app/events/interactionCreate.js
+++ app/events/interactionCreate.js
@@ -1,7 +1,5 @@
 const { Events } = require("discord.js");
 const { handleSelectMenuInteraction } = require("../utils/selectBoxUtils");
-const { handleScheduleSelectInteraction } = require("../utils/scheduleSelectInteraction");
-const { handleScheduleModalInteraction } = require("../utils/scheduleModalInteraction");
 
 /**
  * interactionCreate 이벤트 핸들러
@@ -10,9 +8,7 @@ const { handleScheduleModalInteraction } = require("../utils/scheduleModalIntera
 module.exports = {
 	name: Events.InteractionCreate,
 	once: false,
-	async execute(client, interaction) {
+	async execute(_client, interaction) {
 		if (await handleSelectMenuInteraction(interaction)) return;
-		if (await handleScheduleSelectInteraction(interaction)) return;
-		if (await handleScheduleModalInteraction(interaction, client)) return;
 	},
 };
